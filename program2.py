2fcce158dd773d0935bb3e7572d33106f90a93ff
Ravindra-Singh-Rajput <29ravirajput@gmail.com>
2024-09-27 13:49:38 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -33,12 +33,10 @@ class Solution(object):
             prev_value = current_value
 
         return total
-
-# Test cases
 solution = Solution()
-print(solution.romanToInt("III"))       # Output: 3
-print(solution.romanToInt("LVIII"))     # Output: 58
-print(solution.romanToInt("MCMXCIV"))   # Output: 1994
+print(solution.romanToInt("III"))      
+print(solution.romanToInt("LVIII"))    
+print(solution.romanToInt("MCMXCIV"))  
 
 
 
